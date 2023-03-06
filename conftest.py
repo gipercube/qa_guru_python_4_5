57390b8fc64b80b47ddb9e83f0c54a2dbe4eae08
Aleksandr Kozelskikh <a.n.kozelskikh@gmail.com>
2023-03-06 20:03:49 +0300

    Homework 1.3
    remove ad.banners

--- conftest.py
+++ conftest.py
@@ -11,4 +11,8 @@ def window_size():
 @pytest.fixture()
 def open_browser_for_form(window_size):
     browser.open('https://demoqa.com/automation-practice-form')
+    # remove ad.banners
+    browser.execute_script('document.querySelector("footer").remove()')
+    browser.execute_script('document.querySelector("#fixedban").remove()')
+    browser.execute_script('document.querySelector("#RightSide_Advertisement").remove()')
 
